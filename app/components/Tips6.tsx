1c29d2c68ade9e8323ff0397c08e2f16ce7f29f1
N.I <iworksWeb@gmail.com>
2024-12-28 15:44:32 +0900

    size and space

--- app/components/Tips6.tsx
+++ app/components/Tips6.tsx
@@ -1,7 +1,8 @@
 const Tips6 = () => {
   return (
-    <div className="flex flex-col gap-4">
-      <div className="bg-orange-50  h-20 w-20"></div>
+    // <div className="flex flex-col gap-4">
+    <div className="flex flex-col space-y-4">
+      <div className="bg-orange-500 size-20"></div>
       <div className="bg-orange-500 h-20 w-20"></div>
       <div className="bg-orange-500 h-20 w-20"></div>
       <div className="bg-orange-500 h-20 w-20"></div>
