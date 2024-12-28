2f33aea22e8a30df89a584b096eae2d95c737c7d
N.I <iworksWeb@gmail.com>
2024-12-28 10:03:46 +0900

    tips Animations

--- app/components/Tips1_2.tsx
+++ app/components/Tips1_2.tsx
@@ -3,11 +3,14 @@ import React from "react";
 const Tips1_2 = () => {
   return (
     <div className="flex gap-4">
-      <div className="peer group cursor-pointer grid place-items-center h-40 w-40 bg-blue-400 hover:bg-purple-400">
+      <div className="peer group cursor-pointer grid place-items-center h-40 w-40 bg-blue-400 hover:bg-purple-400 transition-colors duration-500 ease-in-out">
         <div className="h-10 w-10 bg-black group-hover:bg-red-600"></div>
         <div className="h-10 w-10 bg-black group-hover:bg-blue-600"></div>
       </div>
-      <div className="h-40 w-40 bg-green-400 peer-hover:bg-slate-600"></div>
+      {/* https://tailwindcss.com/docs/animation */}
+      <div className="h-40 w-40 bg-green-400 peer-hover:bg-slate-600 animate-bounce"></div>
+      <div className="h-40 w-40 bg-green-400 peer-hover:bg-slate-600 animate-spin"></div>
+      <div className="h-40 w-40 bg-green-400 peer-hover:bg-slate-600 animate-pulse"></div>
     </div>
   );
 };
