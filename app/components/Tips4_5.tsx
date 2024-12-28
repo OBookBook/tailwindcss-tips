ad44e5ad1aea7d2549faddd5d8e20e86a43063b3
N.I <iworksWeb@gmail.com>
2024-12-28 11:24:32 +0900

    apply and theme

--- app/components/Tips4_5.tsx
+++ app/components/Tips4_5.tsx
@@ -3,7 +3,8 @@ import React from "react";
 const Tips4_5 = () => {
   return (
     <div>
-      {/* <div className="sample_css">Subscription</div> */}
+      <div className="sample_css">Subscription</div>
+      <div className="sample_css ml-10">Subscription</div>
 
       <div>{/* <div className="w-20 h-10 rounded-lg"></div> */}</div>
     </div>
